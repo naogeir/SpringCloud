6052c4419991af5d7e8fa87de5d3728acd4a47a4
noageir <noageir@163.com>
2019-03-01 14:39:26 +0800

    整理代码

---
+++ 06_gateway-server/src/main/java/com/micro/system/gateway/GatewayApplication.java
@@ -0,0 +1,21 @@
+package com.micro.system.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author Noageir
+ * Date:2018-05-06 22:20
+ * Project:com.spring.cloud
+ * Package:com.micro.system.eureka
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+
+}
